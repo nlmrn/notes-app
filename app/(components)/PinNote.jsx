1cb4783b6fe4940e3f18b65f4789ab8a704824b7
niel <definitelynotniel@gmail.com>
2024-09-03 15:18:30 +0800

    git

--- app/(components)/PinNote.jsx
+++
@@ -1,9 +0,0 @@
-import { faThumbTack } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-export default function PinNote() {
-  return (
-    <div>
-      <FontAwesomeIcon icon={faThumbTack} className="text-gray-800" />
-    </div>
-  );
-}
